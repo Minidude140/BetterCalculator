2a928085b1cdf6b36d51ec36ec8dc7423bd9fb4d
Zac <zac.minidude140@Gmail.com>
2023-09-10 16:56:33 -0600

    Added operatoin try catch, computed response, and exit prompts

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -19,23 +19,24 @@ Module BetterCalculator
         '[~]Return computated value
         '[~]Prompt to exit program
         '[~]Update prompts to submission instructions
-        '[1/2]Add Loop and Quit Functionality
+        '[~]Add Loop and Quit Functionality
         '[~]Add try catch exeption number one
         '[~]Add try catch exeption number two
-        '[]Add try catch exeption operation
-        '[]Add subract and Divide operations
+        '[~]Add try catch exeption operation
+        '[~]Add subract and Divide operations
 
 
+        Dim exitFlag As Boolean = False
         Dim userInput As String
         Dim numberOne As Integer
         Dim numberTwo As Integer
-        Dim operation As String
-        Dim exitFlag As Boolean = False
-        Dim numOneLoop As Boolean = False
-        Dim numTwoLoop As Boolean = False
-        Dim operationLoop As Boolean = False
+        Dim operation As Integer
 
-        Do While exitFlag = False
+        Do
+            Dim numOneLoop As Boolean = False
+            Dim numTwoLoop As Boolean = False
+            Dim operationLoop As Boolean = False
+
             Console.WriteLine("Please enter two numbers." & vbLf & "Enter Q at anytime to quit.")
 
             'First Number Aquisition
@@ -78,26 +79,58 @@ Module BetterCalculator
                 End Try
             Loop
 
-            [operation] = CStr(userInput)
+            'Prompt, compute, Return
+            Do While operationLoop = False
+                Console.WriteLine("Choose one of the following options:" & vbLf & "1. Add" & vbLf & "2. Subtract" & vbLf & "3.Multiply" & vbLf & "4. Divide")
+                userInput = Console.ReadLine()
+                Try
+                    operation = CInt(userInput)
+                    Select Case operation
+                            'Wrong input repeats loop
+                        Case <= 0
+                            Console.WriteLine($"You entered: {userInput}" & vbLf)
+                            'Wrong input repeats loop
+                        Case >= 5
+                            Console.WriteLine($"You entered: {userInput}" & vbLf)
+                            'Input within range exit loop
+                        Case = 1
+                            operationLoop = True
+                            'Should not ever reach this point
+                        Case Else
+                            Console.WriteLine("This should not have happened...")
+                    End Select
+                Catch ex As Exception
+                    If userInput = "q" Or userInput = "Q" Then
+                        numOneLoop = True
+                        numTwoLoop = True
+                        operationLoop = True
+                        exitFlag = True
+                    Else
+                        Console.WriteLine($"You entered: {userInput}" & vbLf)
+                    End If
+                End Try
+            Loop
+            Select Case operation
 
-            'Prompt, Compute, and Return
-            Console.WriteLine("Would you like to add or multiply these two numbers?" & vbLf & "Please enter '+' or '*'")
-            userInput = Console.ReadLine()
-
-            'Determine Desired Operation
-            If (operation = "+") Then
-                'Add
-                Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}" & vbLf)
-            ElseIf (operation = "*") Then
-                'Multiply
-                Console.WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}" & vbLf)
-            Else
-                'not multiply or add
-                Console.WriteLine($"{numberOne} {userInput} {numberTwo} = ???" & vbLf & "I don't know that kind of math." & vbLf)
-            End If
-
-            'End greeting and exit
-        Loop
+                     'Add two inputs
+                Case = 1
+                    Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}" & vbLf)
+                    'Subtract one from two
+                Case = 2
+                    Console.WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}" & vbLf)
+                    'Multiply two inputs
+                Case = 3
+                    Console.WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}" & vbLf)
+                    'Divide one from two
+                Case = 4
+                    Console.WriteLine($"{numberOne} / {numberTwo} = {numberOne / numberTwo}" & vbLf)
+                    'Should not run this cases oustide range handled in the about try/catch
+                Case Else
+                    Console.WriteLine("This should not have happened...")
+            End Select
+        Loop Until exitFlag = True
+        Console.WriteLine("You entered: q")
+        Console.WriteLine("Have a nice day." & vbLf & "Press enter to close this window.")
         Console.Read()
 
     End Sub
